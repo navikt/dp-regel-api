ed3cbedd4fcf9ff5f9e4f2b1dc854c96e2a261ff
Nico Kuijlen <nico.kuijlen@nav.no>
2019-01-14 12:44:40 +0100

    Refine api with agreed on endpoints and parameters returning dumy data

--- src/main/kotlin/no/nav/dagpenger/regel/api/minsteinntekt/MinsteinntektRoute.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/minsteinntekt/MinsteinntektRoute.kt
@@ -15,8 +15,10 @@ import io.ktor.locations.Location
 import io.ktor.response.header
 import io.ktor.response.respond
 import io.ktor.routing.Routing
-import no.nav.dagpenger.regel.api.MinsteInntektBeregningsRequest
+import mu.KotlinLogging
 import no.nav.dagpenger.regel.api.Regel
+import no.nav.dagpenger.regel.api.grunnlag.Parametere
+import no.nav.dagpenger.regel.api.grunnlag.Utfall
 import no.nav.dagpenger.regel.api.tasks.Tasks
 import no.nav.dagpenger.regel.api.tasks.taskResponseFromTask
 
@@ -25,16 +27,22 @@ import no.nav.dagpenger.regel.api.tasks.taskResponseFromTask
 class PostMinsteinntekt
 
 @Group("Minsteinntekt")
-@Location("/minsteinntekt/{id}")
-data class GetMinsteinntekt(val id: String)
+@Location("/minsteinntekt")
+data class GetMinsteinntektWithAktorId(val aktorId: String)
+
+@Group("Minsteinntekt")
+@Location("/minsteinntekt/{beregningsId}")
+data class GetMinsteinntekt(val beregningsId: String)
+
+private val LOGGER = KotlinLogging.logger {}
 
 fun Routing.minsteinntekt(minsteinntektBeregninger: MinsteinntektBeregninger, tasks: Tasks) {
-    post<PostMinsteinntekt, MinsteInntektBeregningsRequest>(
+    post<PostMinsteinntekt, MinsteinntektBeregningsRequest>(
         "minsteinntektsberegning"
-            .description("Start minsteinntektsberegning")
+            .description("Kjør en beregning av minsteinntekt")
             .examples()
             .responds()
-    ) { _, request ->
+    ) { _, payload ->
         val taskId = tasks.createTask(Regel.MINSTEINNTEKT)
 
         // dette skal egentlig bli gjort av kafka-consumer når regelberegning er ferdig
@@ -44,18 +52,66 @@ fun Routing.minsteinntekt(minsteinntektBeregninger: MinsteinntektBeregninger, ta
         call.respond(HttpStatusCode.Accepted, taskResponseFromTask(tasks.getTask(taskId)))
     }
 
+    get<GetMinsteinntektWithAktorId>(
+        "hent alle minsteinntektsberegninger for aktør"
+            .description("??")
+            .examples()
+            .responds()
+    ) { param ->
+        LOGGER.info { param.aktorId }
+        call.respond(minsteinntektBeregninger.getBeregningForAktorId(param.aktorId))
+    }
+
     get<GetMinsteinntekt>(
-        "resultat av minsteinntektsberegning".responds(
-            ok<MinsteinntektBeregningResultat>(
-                example(
-                    "model",
-                    MinsteinntektBeregningResultat.exampleInntektBeregning
+        "resultat av minsteinntektsberegning"
+            .responds(
+                ok<MinsteinntektBeregningsResult>(
+                    example(
+                        "model",
+                        MinsteinntektBeregningsResult.exampleInntektBeregning
+                    )
                 )
             )
-        )
     ) { param ->
-        val id = param.id
+        LOGGER.info { param.beregningsId }
+        val id = param.beregningsId
 
-        call.respond(minsteinntektBeregninger.getBeregning(id))
+        call.respond(minsteinntektBeregninger.getBeregningForBeregningsId(id))
     }
 }
+
+data class MinsteinntektBeregningsRequest(
+    val aktorId: String,
+    val vedtakId: Int,
+    val beregningsdato: String,
+    val inntektsId: String,
+    val bruktinntektsPeriode: InntektsPeriode,
+    val harAvtjentVerneplikt: Boolean,
+    val oppfyllerKravTilFangstOgFisk: Boolean,
+    val harArbeidsperiodeEosSiste12Maaneder: Boolean
+)
+
+data class MinsteinntektBeregningsResult(
+    val beregningsId: String,
+    val utfall: Utfall,
+    val opprettet: String,
+    val utfort: String,
+    val parametere: Parametere,
+    val harAvtjentVerneplikt: Boolean,
+    val oppfyllerKravTilFangstOgFisk: Boolean,
+    val harArbeidsperiodeEosSiste12Maaneder: Boolean
+) {
+    companion object {
+        val exampleInntektBeregning = mapOf(
+            "oppfyllerMinsteinntekt" to true,
+            "status" to 1
+        )
+    }
+}
+
+data class InntektsPeriode(
+    val foersteMaaned: String,
+    val sisteMaaned: String
+)
+
+class InvalidInputException(override val message: String) : RuntimeException(message)
