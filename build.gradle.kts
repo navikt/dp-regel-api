b5061433a2706657ae8b35d4ad63ec46b8390370
Geir André Lund <geir.andre.lund@nav.no>
2019-05-13 15:54:29 +0200

    Add log4j2 prometheus appender - Add counter for Log levels to the app Ref https://github.com/prometheus/client_java#logging
    Example output
    ```
    # HELP log4j2_appender_total Log4j2 log statements at various log levels
    # TYPE log4j2_appender_total counter
    log4j2_appender_total{level="debug",} 0.0
    log4j2_appender_total{level="warn",} 2.0
    log4j2_appender_total{level="trace",} 0.0
    log4j2_appender_total{level="error",} 0.0
    log4j2_appender_total{level="fatal",} 0.0
    log4j2_appender_total{level="info",} 17.0
    ``

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     implementation("no.bekk.bekkopen:nocommons:0.8.2")
     implementation("io.prometheus:simpleclient_common:$prometheusVersion")
     implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
+    implementation("io.prometheus:simpleclient_log4j2:$prometheusVersion")
 
     runtime("com.bettercloud:vault-java-driver:$vaultJavaDriverVersion")
 
