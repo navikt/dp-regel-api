c4e4712b9e6bd1fc32b31b4fe46e6943b36d5ed6
Geir André Lund <geir.andre.lund@nav.no>
2019-05-27 13:56:19 +0200

    Calculate 'sisteInntektsmåned' aka 'sisteAvsluttendeKalenderMåned' different. This functionality is replaced and will be handled by dp-inntekt-api. This is an temporary fix to not get any displacement in the way we calculate inntekt

--- src/main/kotlin/no/nav/dagpenger/regel/api/SenesteInntektsmåned.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/SenesteInntektsmåned.kt
@@ -7,16 +7,29 @@ import java.time.ZoneId
 import java.util.Date
 
 internal fun senesteInntektsmåned(beregningsdato: LocalDate): YearMonth {
-    val firstWorkingDayAfterFifth = firstWorkingDayAfterFifth(beregningsdato.monthValue, beregningsdato.year)
-
-    return if (beregningsdato.isBefore(firstWorkingDayAfterFifth)) YearMonth.of(beregningsdato.minusMonths(2).year, beregningsdato.minusMonths(2).month) else YearMonth.of(beregningsdato.minusMonths(1).year, beregningsdato.minusMonths(1).month)
+    return Opptjeningsperiode(beregningsdato).sisteAvsluttendeKalenderMåned
 }
 
-internal fun firstWorkingDayAfterFifth(month: Int, year: Int): LocalDate {
-    var firstWorkingDayAfterFifth: LocalDate = LocalDate.of(year, month, 6)
-    while (!isWorkingDay(firstWorkingDayAfterFifth)) firstWorkingDayAfterFifth = firstWorkingDayAfterFifth.plusDays(1)
-    return firstWorkingDayAfterFifth
-}
+// TODO: temporary - replaced by functionality from dp-inntekt-api - need a two step operation for now
+data class Opptjeningsperiode(val beregningsdato: LocalDate) {
+    private val antattRapporteringsFrist = LocalDate.of(beregningsdato.year, beregningsdato.month, 5)
+    private val reellRapporteringsFrist: LocalDate =
+        finnFørsteArbeidsdagEtterRapporterteringsFrist(antattRapporteringsFrist)
+    private val månedSubtraksjon: Long = when {
+        beregningsdato.isBefore(reellRapporteringsFrist) || beregningsdato.isEqual(reellRapporteringsFrist) -> 2
+        else -> 1
+    }
 
-internal fun isWorkingDay(localDate: LocalDate): Boolean =
-        NorwegianDateUtil.isWorkingDay(Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant()))
+    val sisteAvsluttendeKalenderMåned: YearMonth = beregningsdato.minusMonths(månedSubtraksjon).toYearMonth()
+
+    private fun finnFørsteArbeidsdagEtterRapporterteringsFrist(rapporteringsFrist: LocalDate): LocalDate {
+        return if (rapporteringsFrist.erArbeidsdag()) rapporteringsFrist else finnFørsteArbeidsdagEtterRapporterteringsFrist(
+            rapporteringsFrist.plusDays(1)
+        )
+    }
+
+    private fun LocalDate.erArbeidsdag(): Boolean =
+        NorwegianDateUtil.isWorkingDay(Date.from(this.atStartOfDay(ZoneId.systemDefault()).toInstant()))
+
+    private fun LocalDate.toYearMonth(): YearMonth = YearMonth.of(this.year, this.month)
+}
