3fec48454711a7e82532cbb5e429023a70041683
Giao The Cung <giaothe@gmail.com>
2019-06-20 14:38:20 +0200

    Auth on subsumsjon route.

--- src/main/kotlin/no/nav/dagpenger/regel/api/routing/SubsumsjonRoute.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/routing/SubsumsjonRoute.kt
@@ -1,6 +1,7 @@
 package no.nav.dagpenger.regel.api.routing
 
 import io.ktor.application.call
+import io.ktor.auth.authenticate
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.routing.Route
@@ -10,11 +11,13 @@ import no.nav.dagpenger.regel.api.BadRequestException
 import no.nav.dagpenger.regel.api.db.SubsumsjonStore
 
 internal fun Route.subsumsjon(store: SubsumsjonStore) {
-    route("subsumsjon/") {
-        get("/{subsumsjonsid}") {
-            val subsumsjonsId = call.parameters["subsumsjonsid"] ?: throw BadRequestException()
-            store.getSubsumsjon(subsumsjonsId).toJson().let {
-                call.respond(HttpStatusCode.OK, it)
+    authenticate {
+        route("subsumsjon/") {
+            get("/{subsumsjonsid}") {
+                val subsumsjonsId = call.parameters["subsumsjonsid"] ?: throw BadRequestException()
+                store.getSubsumsjon(subsumsjonsId).toJson().let {
+                    call.respond(HttpStatusCode.OK, it)
+                }
             }
         }
     }
