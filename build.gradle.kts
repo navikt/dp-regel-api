0187d5b34be6b41924e8d919880c70c46bc99351
Geir André Lund <geir.andre.lund@nav.no>
2019-07-08 12:32:35 +0200

    Use defined dependencies from monorepo

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,20 +3,24 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     application
-    kotlin("jvm") version "1.3.21"
-    id("com.diffplug.gradle.spotless") version "3.13.0"
-    id("com.github.johnrengelman.shadow") version "4.0.3"
+    kotlin("jvm") version Kotlin.version
+    id(Spotless.spotless) version Spotless.version
+    id(Shadow.shadow) version Shadow.version
+}
+
+buildscript {
+    repositories {
+        jcenter()
+    }
 }
 
 apply {
-    plugin("com.diffplug.gradle.spotless")
+    plugin(Spotless.spotless)
 }
 
 repositories {
-    mavenCentral()
     jcenter()
     maven("http://packages.confluent.io/maven/")
-    maven("https://oss.sonatype.org/content/repositories/snapshots/")
     maven("https://jitpack.io")
 }
 
@@ -29,25 +33,6 @@ java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
-val ktorVersion = "1.2.0"
-val kotlinLoggingVersion = "1.6.22"
-val log4j2Version = "2.11.1"
-val jupiterVersion = "5.3.2"
-val kafkaVersion = "2.0.1"
-val confluentVersion = "5.0.2"
-val moshiVersion = "1.8.0"
-val ktorMoshiVersion = "1.0.1"
-val testcontainers_version = "1.11.2"
-val flywayVersion = "6.0.0-beta"
-val hikariVersion = "3.3.1"
-val postgresVersion = "42.2.5"
-val vaultJdbcVersion = "1.3.1"
-val kotliqueryVersion = "1.3.0"
-val vaultJavaDriverVersion = "3.1.0"
-val konfigVersion = "1.6.10.0"
-val mockkVersion = "1.9.3"
-val prometheusVersion = "0.6.0"
-val dpBibliotekerVersion = "2019.06.19-09.38.5466af242e44"
 
 configurations {
     this.all {
@@ -57,54 +42,67 @@ configurations {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.github.navikt:dagpenger-events:2019.06.12-14.01.4b1e1a663635")
-    implementation("com.github.navikt:dagpenger-streams:2019.06.26-21.57.bdd7e296c753")
-    implementation("com.github.navikt.dp-biblioteker:ktor-utils:$dpBibliotekerVersion")
-    implementation("io.ktor:ktor-server:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty:$ktorVersion")
-    implementation("io.ktor:ktor-auth:$ktorVersion")
-    implementation("io.ktor:ktor-locations:$ktorVersion")
-    implementation("io.ktor:ktor-metrics-micrometer:$ktorVersion")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.1.5")
-    implementation("com.squareup.moshi:moshi-adapters:$moshiVersion")
-    implementation("com.squareup.moshi:moshi-kotlin:$moshiVersion")
-    implementation("com.squareup.moshi:moshi:$moshiVersion")
-    implementation("com.ryanharter.ktor:ktor-moshi:$ktorMoshiVersion")
-    implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
-    implementation("org.apache.logging.log4j:log4j-api:$log4j2Version")
-    implementation("org.apache.logging.log4j:log4j-core:$log4j2Version")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4j2Version")
-    implementation("com.vlkan.log4j2:log4j2-logstash-layout-fatjar:0.15")
-    implementation("org.slf4j:slf4j-api:1.7.25")
-    implementation("de.huxhorn.sulky:de.huxhorn.sulky.ulid:8.2.0")
-    implementation("org.apache.kafka:kafka-clients:$kafkaVersion")
-    implementation("org.apache.kafka:kafka-streams:$kafkaVersion")
-    implementation("io.confluent:kafka-streams-avro-serde:$confluentVersion")
-    implementation("org.flywaydb:flyway-core:$flywayVersion")
-    implementation("com.zaxxer:HikariCP:$hikariVersion")
-    implementation("org.postgresql:postgresql:$postgresVersion")
-    implementation("com.github.seratch:kotliquery:$kotliqueryVersion")
-    implementation("com.natpryce:konfig:$konfigVersion")
-    implementation("no.nav:vault-jdbc:$vaultJdbcVersion") {
+    implementation(Dagpenger.Streams)
+    implementation(Dagpenger.Events)
+
+    implementation(Kafka.clients)
+    implementation(Kafka.streams)
+
+    implementation(Ktor.server)
+    implementation(Ktor.serverNetty)
+    implementation(Ktor.auth)
+    implementation(Ktor.authJwt)
+    implementation(Ktor.locations)
+    implementation(Ktor.micrometerMetrics)
+    implementation(Dagpenger.Biblioteker.ktorUtils)
+    implementation(Micrometer.prometheusRegistry)
+
+    implementation(Moshi.moshi)
+    implementation(Moshi.moshiAdapters)
+    implementation(Moshi.moshiKotlin)
+    implementation(Moshi.moshiKtor)
+
+    implementation(Log4j2.api)
+    implementation(Log4j2.core)
+    implementation(Log4j2.slf4j)
+    implementation(Log4j2.Logstash.logstashLayout)
+    implementation(Kotlin.Logging.kotlinLogging)
+
+    implementation(Slf4j.api)
+
+    implementation(Ulid.ulid)
+
+    implementation(Dagpenger.Streams)
+    implementation(Dagpenger.Events)
+
+    implementation(Database.Flyway)
+    implementation(Database.HikariCP)
+    implementation(Database.Postgres)
+    implementation(Database.Kotlinquery)
+    implementation(Konfig.konfig)
+    implementation(Database.VaultJdbc) {
         exclude(module = "slf4j-simple")
         exclude(module = "slf4j-api")
     }
-    implementation("io.prometheus:simpleclient_common:$prometheusVersion")
-    implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
-    implementation("io.prometheus:simpleclient_log4j2:$prometheusVersion")
 
-    runtime("com.bettercloud:vault-java-driver:$vaultJavaDriverVersion")
+    implementation(Prometheus.common)
+    implementation(Prometheus.hotspot)
+    implementation(Prometheus.log4j2)
+
+    runtime(Vault.javaDriver)
 
     testImplementation(kotlin("test"))
-    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$jupiterVersion")
-    testImplementation("org.apache.kafka:kafka-streams-test-utils:$kafkaVersion")
-    testImplementation("org.testcontainers:postgresql:$testcontainers_version")
-    testImplementation("org.testcontainers:kafka:$testcontainers_version")
-    testImplementation("io.mockk:mockk:$mockkVersion")
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jupiterVersion")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$jupiterVersion")
+    testImplementation(Ktor.ktorTest)
+    testImplementation(Junit5.api)
+    testImplementation(Junit5.kotlinRunner)
+    testImplementation(TestContainers.postgresql)
+    testImplementation(TestContainers.kafka)
+    testImplementation(Kafka.streamTestUtils)
+
+    testImplementation(Mockk.mockk)
+
+    testRuntimeOnly(Junit5.engine)
+    testRuntimeOnly(Junit5.vintageEngine)
 }
 
 spotless {
