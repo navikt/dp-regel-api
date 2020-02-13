12e6bac790440073a0e75e6d80319e9bdf225c3b
Geir André Lund <geir.andre.lund@nav.no>
2020-02-13 21:03:05 +0100

    Trigger build

--- src/main/kotlin/no/nav/dagpenger/regel/api/RegelApi.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/RegelApi.kt
@@ -63,7 +63,6 @@ fun main() = runBlocking {
     val subsumsjonStore = PostgresSubsumsjonStore(dataSource)
     val bruktSubsumsjonStore = PostgresBruktSubsumsjonStore(dataSource)
     val vaktmester = Vaktmester(dataSource = dataSource, subsumsjonStore = subsumsjonStore)
-
     /*
     fixedRateTimer(
         name = "vaktmester",
