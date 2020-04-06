d647e3dee839a45bed8cdd2688f27be0805af4d1
Geir André Lund <geir.andre.lund@nav.no>
2020-04-06 11:43:50 +0200

    Revert "Revert "Disable subsumsjonBrukt consumer.""
    This reverts commit 09285418f2d2978a1057b310d079a26c29ed18a0.

--- src/main/kotlin/no/nav/dagpenger/regel/api/RegelApi.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/RegelApi.kt
@@ -24,7 +24,6 @@ import io.micrometer.core.instrument.Clock
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import io.prometheus.client.CollectorRegistry
-import kotlinx.coroutines.cancel
 import mu.KotlinLogging
 import no.finn.unleash.Unleash
 import no.nav.dagpenger.ktor.auth.apiKeyAuth
@@ -44,7 +43,6 @@ import no.nav.dagpenger.regel.api.routing.naischecks
 import no.nav.dagpenger.regel.api.routing.subsumsjon
 import no.nav.dagpenger.regel.api.streams.DagpengerBehovProducer
 import no.nav.dagpenger.regel.api.streams.KafkaDagpengerBehovProducer
-import no.nav.dagpenger.regel.api.streams.KafkaSubsumsjonBruktConsumer
 import no.nav.dagpenger.regel.api.streams.KafkaSubsumsjonConsumer
 import no.nav.dagpenger.regel.api.streams.SubsumsjonPond
 import no.nav.dagpenger.regel.api.streams.producerConfig
@@ -78,14 +76,6 @@ fun main() {
             it.start()
         }
 
-    val bruktSubsumsjonConsumer = KafkaSubsumsjonBruktConsumer.apply {
-        create(
-            config = config,
-            bruktSubsumsjonStore = bruktSubsumsjonStore,
-            vaktmester = vaktmester
-        )
-        listen()
-    }
     val kafkaProducer = KafkaDagpengerBehovProducer(
         producerConfig(
             config.application.id,
@@ -108,8 +98,7 @@ fun main() {
                 subsumsjonStore as HealthCheck,
                 bruktSubsumsjonStore as HealthCheck,
                 kafkaConsumer as HealthCheck,
-                kafkaProducer as HealthCheck,
-                bruktSubsumsjonConsumer as HealthCheck
+                kafkaProducer as HealthCheck
             )
         )
     }.also {
@@ -118,7 +107,6 @@ fun main() {
 
     Runtime.getRuntime().addShutdownHook(Thread {
         kafkaConsumer.stop()
-        bruktSubsumsjonConsumer.cancel()
         app.stop(10, 60, TimeUnit.SECONDS)
     })
 }
@@ -144,8 +132,8 @@ internal fun Application.api(
 
         filter { call ->
             !call.request.path().startsWith("/isAlive") &&
-                    !call.request.path().startsWith("/isReady") &&
-                    !call.request.path().startsWith("/metrics")
+                !call.request.path().startsWith("/isReady") &&
+                !call.request.path().startsWith("/metrics")
         }
     }
 
